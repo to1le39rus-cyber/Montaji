354f93fca7ceac171b321f2b4ac55cff65516b7b
to1le39rus-cyber <to1le39rus@gmail.com>
2026-08-12 09:39:48 +0200

    chore: remove legacy secure app

--- secure-app-v2/config.js
+++
@@ -1,8 +0,0 @@
-window.APP_CONFIG = {
-  supabaseUrl: ['https://','ijiekvurwnwrpvaxkdvn','.supabase.co'].join(''),
-  supabaseKey: [
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
-    'eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqaWVrdnVyd253cnB2YXhrZHZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNjIyMDAsImV4cCI6MjA4OTgzODIwMH0',
-    'qOkchV3RDmkDfAK8IrH1PCynPYy8KBUM5C9oIT0UdWE'
-  ].join('.')
-};
