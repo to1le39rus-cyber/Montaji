6e41006307043b9bc990ad38056be1a57c5a4852
to1le39rus-cyber <to1le39rus@gmail.com>
2026-09-23 00:59:45 +0200

    feat(montra): add canonical master mark

---
+++ src/ui/brand.js
@@ -0,0 +1,5 @@
+export const montraMark = ({className='montra-mark', title='MONTRA'}={}) => `
+<svg class="${className}" viewBox="0 0 100 100" role="img" aria-label="${title}" xmlns="http://www.w3.org/2000/svg">
+  <path class="montra-panel montra-panel-left" d="M10 82V27L46 45V78L34 72V50L22 44V82Z"/>
+  <path class="montra-panel montra-panel-right" d="M90 82V27L54 45V78L66 72V50L78 44V82Z"/>
+</svg>`;
